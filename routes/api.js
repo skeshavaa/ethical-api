43736bdafce1cbde1ac7b30f09e1e9777b213dea
anjali-001 <anjali001.as@gmail.com>
2021-03-30 22:51:34 +0530

    removed useless comments

--- routes/api.js
+++ routes/api.js
@@ -3,11 +3,7 @@ const Item = require('../models/items');
 const router = express.Router();
 const uniqid = require('uniqid');
 
-
-// Route Get Data
-// This is a sample and needs to be changed
 router.get('/getAll', (req, res) => {
-    // items.find().then(item => res.json(item))
     Item.findOne({token:req.body.token, _id:req.body._id}, (err,doc)=>{
         if(doc){
             console.log('Matched!');
